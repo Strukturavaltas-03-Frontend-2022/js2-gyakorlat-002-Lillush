93adfb5a2fae7678105f4dbd490e85248a43db58
Lillush <lillush88@gmail.com+>
2022-08-21 14:29:50 +0200

    mostmárkész

--- 01/solution/js/summarize.js
+++ 01/solution/js/summarize.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const summarize = (...numbers) => {
-   const nums = [...numbers].map(number => Number.isInteger(number));
+   const nums = [...numbers].filter(number => Number.isInteger(number));
+   let numArr;
         
+   if (nums.some((number) => !Number.isSafeInteger(number))) {
+      numArr = nums.map((number) => BigInt(number));
+   } else {
+      numArr = nums.map((number) => Number(number));
+   }
 
-
-
+   const sum = numArr.reduce((acc, cur) => (acc += cur));
+   return Number.isSafeInteger(sum) ? sum : BigInt(sum);
 
 }
 
@@ -13,4 +19,4 @@ const summarize = (...numbers) => {
 
 
 
-// export default summarize;
+export default summarize;
